901cad079cdfd90c661c5258c4e60d7fb859c290
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2013-01-22 01:01:33 +0100

    initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b6fa5e140cb0149cb0734c46ff385b68cd22021050ba57274b8536ba504d8544303526a1fb47f906f53127b5e03a8abd9e875d815e53f80b750110dad2ba1b6b'
